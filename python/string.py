c2c749aa6f5a745bd6f02d08f222b913183ab1af
Grace Liu <graceliu@Yans-MBP.localdomain>
2020-03-23 21:15:01 -0400

    I finished my first day

---
+++ python/string.py
@@ -0,0 +1,8 @@
+print("Today is a bad day")
+print('I like cheese')
+print("Why are his strings so long don't")
+print('Dad once said, "Wake up"')
+print("hello, "+"world")
+greeting= "Hello, "
+name = input("Please enter your name: ")
+print(greeting + name)
