9507336c3116d08014e05127208f7d38cf1011db
Grace Liu <graceliu@Yans-MBP.localdomain>
2020-03-24 21:32:48 -0400

    finsihed day 2

--- python/string.py
+++ python/string.py
@@ -4,5 +4,16 @@ print("Why are his strings so long don't")
 print('Dad once said, "Wake up"')
 print("hello, "+"world")
 greeting= "Hello, "
-name = input("Please enter your name: ")
+#name = input("Please enter your name: ")
+name = "Tim"
 print(greeting + " "+ name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(name + " is " + age + " years old")
+print(type(age))
