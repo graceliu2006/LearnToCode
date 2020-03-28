a146fbd29563e8c8695a451977396a78ecf7bb20
Grace Liu <graceliu@Yans-MBP.localdomain>
2020-03-27 21:45:50 -0400

    finished birthday (day 4)

---
+++ python/sliceback.py
@@ -0,0 +1,8 @@
+letters="abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[::-1]
+print(backwards)
+
+print(letters[-10:-13:-1])
+print(letters[4::-1])
+print(letters[:-9:-1])
--- python/strings2.py
+++ python/strings2.py
@@ -1,12 +1,22 @@
+
+
 parrot="Norwegian Blue"
 
-print(parrot)
+print(parrot[0:6:2]) #NRE
+print(parrot[:6:3]) #Nw
+
+number="9,223;372:036 854,775;807"
+seperators =number[1::4]
+print(seperators)
+
+values="".join(char if char not in seperators else " " for char in number).split()
+
+print(values)
+print([int(val) for val in values])
 
 
 
-print(parrot[3])
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
+
+
+
+
