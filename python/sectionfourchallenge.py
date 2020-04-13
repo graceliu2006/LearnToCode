bee7be0b75ad1347046a77eee1e6f572399b9560
Grace Liu <graceliu@Yans-MBP.localdomain>
2020-04-12 21:28:00 -0400

    Finished HW2

--- python/sectionfourchallenge.py
+++ python/sectionfourchallenge.py
@@ -1,21 +1,19 @@
-option_list = ["sleep","nap","slumber","snooze","doze","siesta","hibernate"]
-chosen = ""
+def choices_function(option_list):
+    chosen = ""
 
-print("Please choose your option from the list below:")
-i=1
+    print("Please choose your option from the list below:")
+    i=1
 
-for option in option_list:
-    print(str(i) + ". " + option)
-    i += 1
-print("0. exit")
+    for option in option_list:
+        print(str(i) + ". " + option)
+        i += 1
+    print("0. exit")
 
-while chosen != 0:
-    chosen = int(input("Please choose an option: "))
-    #if chosen in range (0,len(option_list)+1):
-    if chosen in range (0,i):
-        print("You chose option {}".format(chosen))
-        
+    while chosen != 0:
+        chosen = int(input("Please choose an option: "))
+        #if chosen in range (0,len(option_list)+1):
+        if chosen in range (0,i):
+            print("You chose option {}".format(chosen))
+            
 
-
-
-
+choices_function(["sleep","nap","slumber","snooze","doze","siesta","hibernate"])
